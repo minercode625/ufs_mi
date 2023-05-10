61de2256bbbdcd3776398461d5f8542b9947f102
minercode625 <seowangduk@gmail.com>
2023-05-11 00:44:01 +0900

    exp_2

--- tool/kmeans_rng.m
+++ tool/kmeans_rng.m
@@ -1,14 +1,15 @@
-function [m_res, s_res] = kmeans_rng(X, Y, trial, X_origin)
+function [res, s_res] = kmeans_rng(X, Y, trial, X_origin)
 rng(1);
 cluster = length(unique(Y));
-res = zeros(trial, 4);
+c_res = zeros(trial, 1);
+res = zeros(1, 4);
+res(1, 2) = ent(X);
+res(1, 3) = rep_ent(X);
+res(1, 4) = ffei(X, X_origin);
 for k = 1:trial
     pred = kmeans(X, cluster);
-    res(k, 1) = c_sep(X, pred);
-    res(k, 2) = ent(X);
-    res(k, 3) = rep_ent(X);
-    res(k, 4) = ffei(X, X_origin);
+    c_res(k, 1) = c_sep(X, pred);
 end
-m_res = mean(res);
-s_res = std(res);
+res(1, 1) = mean(c_res);
+s_res = std(c_res);
 end
