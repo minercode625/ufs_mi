a255c02eb83717ef71df0abcda770e4774c398f0
mindercode625 <seowangduk@gmail.com>
2023-05-10 10:57:11 +0900

    error revised

--- tool/ffei.m
+++ tool/ffei.m
@@ -2,22 +2,22 @@ function res = ffei(x, x_origin)
 % Calulate the Fuzzy Feature Evalutaion index
 
 % Calculate the distance between patterns in x and x_origin space
-dist_x = pdist(x)
-dist_xo = pdist(x_origin)
+dist_x = pdist(x);
+dist_xo = pdist(x_origin);
+d_x_max = max(dist_x);
+d_xo_max = max(dist_xo);
 
-d_x_max = max(dist_x)
-d_xo_max = max(dist_xo)
-
-n = size(x, 1)
+n = size(x, 1);
 res = 0;
 for i = 1:n
     for j = 1:n
-      if j == i
-        continue;
+        if j == i
+            continue;
+        end
         ur = 1 - dist_x(i, j)/d_x_max;
         uo = 1 - dist_xo(i, j)/d_xo_max;
         temp = ur * (1 - uo) + uo * (1 - ur);
-        temp = temp * 0.5
+        temp = temp * 0.5;
         res = res + temp;
     end
 end
