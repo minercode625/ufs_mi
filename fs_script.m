3d79990a94dc2840451d0f77d4f0311b5489c5fd
minercode625 <seowangduk@gmail.com>
2023-07-02 14:45:51 +0900

    Rank ufs

--- fs_script.m
+++ fs_script.m
@@ -16,24 +16,23 @@ save_dir = './result_fs/';
 rep_size = 50;
 max_fs_size = 300;
 
-for k = 9:length(data_list)
-    fprintf('%d Start\n', k);
+for k = 1:length(data_list)
+    k
     load(data_list{k});
     data_name = data_list{k}(1:end-4);
     load(strcat(data_name, '_exp.mat'));
     [~, col] = size(fea);
     max_col = min(max_fs_size, col);
-    fs_size = 50:50:max_col;
+    fs_size = 50:25:max_col;
     res = struct('alg', {}, 'acc', {}, 'acc_std', {}, 'nmi', {}, 'nmi_std', {});
-    parfor alg_idx = 1:length(param_struct)
-        alg_idx
+    for alg_idx = 1:length(param_struct)
         alg = param_struct(alg_idx).alg;
         param = param_struct(alg_idx).param;
         acc_table = zeros(length(param), length(fs_size));
         nmi_table = zeros(length(param), length(fs_size));
-        for param_idx = 1:length(param)
-            acc_std_table = zeros(length(param), length(fs_size));
-            nmi_std_table = zeros(length(param), length(fs_size));
+        acc_std_table = zeros(length(param), length(fs_size));
+        nmi_std_table = zeros(length(param), length(fs_size));
+        parfor param_idx = 1:length(param)
             fs_list = param_struct(alg_idx).fea;
             f_sel = fs_list(param_idx,:);
             for fs_idx = 1:length(fs_size)
